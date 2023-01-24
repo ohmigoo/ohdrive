609aa082ec2b162841ee001f30a046e64f3cf0d7
ohmigoo <48247501+ohmigoo@users.noreply.github.com>
2023-01-25 04:03:48 +0700

    Update SwitchLang.tsx

--- components/SwitchLang.tsx
+++ components/SwitchLang.tsx
@@ -25,8 +25,8 @@ const localeText = (locale: string): string => {
       return '🇮🇳 हिन्दी'
     case 'tr-TR':
       return '🇹🇷 Türkçe'
-    case 'zh-TW':
-      return '🇹🇼 繁體中文'
+    case 'id-ID':
+      return 'id Indonesia'
     case 'id-ID':
       return 'id Indonesia'
     default:
